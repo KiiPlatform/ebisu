f3be7f658d4002e35736cf31933012e1436ae86e
apue <baresi@gmail.com>
2014-09-16 21:38:00 +0800

    TianYang: add YouWill sandbox provider

--- YouWill/AppStoreSDK/YouWillSandbox/src/com/youwill/store/sandbox/MainActivity.java
+++ YouWill/AppStoreSDK/YouWillSandbox/src/com/youwill/store/sandbox/MainActivity.java
@@ -2,14 +2,29 @@ package com.youwill.store.sandbox;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    private EditText packageNameEdit;
+
+    private EditText keyEdit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.confirm).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.confirm:
+                Settings.saveAppKey(this, packageNameEdit.getText().toString(),
+                        keyEdit.getText().toString());
+                break;
+        }
+    }
 }
