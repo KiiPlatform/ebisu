eda11f7d73414d9e811926e5415c6889ccdec703
apue <baresi@gmail.com>
2014-09-24 10:25:31 +0800

    TianYang: add recycler view

--- YouWill/AppStore/src/com/youwill/store/fragments/HotFragment.java
+++ YouWill/AppStore/src/com/youwill/store/fragments/HotFragment.java
@@ -1,10 +1,48 @@
 package com.youwill.store.fragments;
 
+import com.youwill.store.R;
+
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by tian on 14-9-23:下午11:01.
  */
-public class HotFragment extends Fragment {
+public class HotFragment extends Fragment implements View.OnClickListener {
 
+    RecyclerView latestView;
+
+    RecyclerView goodView;
+
+    LinearLayoutManager mLinearLayoutManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_hot, container, false);
+        initView(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        latestView = (RecyclerView) view.findViewById(R.id.latest_horizontal_view);
+        goodView = (RecyclerView) view.findViewById(R.id.good_horizontal_view);
+        view.findViewById(R.id.show_all_good).setOnClickListener(this);
+        view.findViewById(R.id.show_all_new).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.show_all_good:
+                break;
+            case R.id.show_all_new:
+                break;
+        }
+    }
 }
