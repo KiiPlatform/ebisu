3a6f2b5687b7db89fc2be71fb52345cc1571258b
apue <baresi@gmail.com>
2014-10-23 00:10:24 +0800

    TianYang: fix search interaction

--- YouWill/AppStore/src/com/youwill/store/MainActivity.java
+++ YouWill/AppStore/src/com/youwill/store/MainActivity.java
@@ -13,6 +13,8 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
@@ -79,6 +81,33 @@ public class MainActivity extends Activity implements View.OnClickListener {
         deleteButton = header.findViewById(R.id.delete_button);
         searchButton.setOnClickListener(this);
         deleteButton.setOnClickListener(this);
+        deleteButton.requestFocus();
+        searchEdit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus && !mSearchFragment.isAdded()) {
+                    showSearchFragment();
+                }
+            }
+        });
+        searchEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null) {
+                    mSearchFragment.beginSearch(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     @Override
@@ -130,6 +159,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
 
     private void switchFragment(View v) {
+        if (mSearchFragment.isAdded()) {
+            getFragmentManager().popBackStack();
+        }
         if (currentFragmentIndex == v.getId()) {
             return;
         }
