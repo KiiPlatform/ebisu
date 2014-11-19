29e2d351304d3b6bc945effa1f013ee772302309
Evan JIANG <firstfan@gmail.com>
2014-11-19 22:29:28 +0800

    Add iOS version YanKon project

---
+++ YanKon/iOS/YanKon/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  YanKon
+//
+//  Created by Evan JIANG on 14/11/19.
+//  Copyright (c) 2014年 Kii Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ YanKon/iOS/YanKon/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  YanKon
+//
+//  Created by Evan JIANG on 14/11/19.
+//  Copyright (c) 2014年 Kii Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
