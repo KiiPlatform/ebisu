ff2706eb36c7c8c492fa207044e1f20249e8fbbd
apue <baresi@gmail.com>
2014-11-27 20:21:59 +0800

    TianYang: commit for color picker

--- YanKon/Android/src/com/kii/yankon/ColorPickerActivity.java
+++ YanKon/Android/src/com/kii/yankon/ColorPickerActivity.java
@@ -1,34 +1,25 @@
 package com.kii.yankon;
 
 import com.larswerkman.holocolorpicker.ColorPicker;
-import com.larswerkman.holocolorpicker.OpacityBar;
-import com.larswerkman.holocolorpicker.SVBar;
-import com.larswerkman.holocolorpicker.SaturationBar;
-import com.larswerkman.holocolorpicker.ValueBar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
-public class ColorPickerActivity extends Activity implements ColorPicker.OnColorChangedListener {
+public class ColorPickerActivity extends Activity implements ColorPicker.OnColorChangedListener, View.OnClickListener {
+
+    private static final String TAG = ColorPickerActivity.class.getSimpleName();
 
     @InjectView(R.id.picker)
     ColorPicker mPicker;
 
-    @InjectView(R.id.svbar)
-    SVBar mSVBar;
-
-    @InjectView(R.id.opacitybar)
-    OpacityBar mOpacityBar;
-
-    @InjectView(R.id.saturationbar)
-    SaturationBar mSaturationBar;
-
-    @InjectView(R.id.valuebar)
-    ValueBar mValueBar;
+    @InjectView(R.id.confirm_button)
+    View mConfirmButton;
 
     private int mColorValue;
 
@@ -37,16 +28,26 @@ public class ColorPickerActivity extends Activity implements ColorPicker.OnColor
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
         ButterKnife.inject(this);
-        mPicker.addSVBar(mSVBar);
-        mPicker.addOpacityBar(mOpacityBar);
-        mPicker.addSaturationBar(mSaturationBar);
-        mPicker.addValueBar(mValueBar);
-        mPicker.setOldCenterColor(mPicker.getColor());
         mPicker.setOnColorChangedListener(this);
+        mConfirmButton.setOnClickListener(this);
     }
 
     @Override
     public void onColorChanged(int i) {
         mColorValue = i;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.confirm_button:
+                Intent intent = new Intent();
+                intent.putExtra("color", mColorValue);
+                setResult(RESULT_OK, intent);
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
