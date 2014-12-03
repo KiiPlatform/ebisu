d5a370a4b2b5ab79a551ad7520b94907d8831d4f
apue <baresi@gmail.com>
2014-12-03 21:57:01 +0800

    TianYang: tmp commit

---
+++ YouWill/AppStore/src/com/youwill/store/activities/PaymentSelectorActivity.java
@@ -0,0 +1,15 @@
+package com.youwill.store.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tian on 14/12/3:上午11:53.
+ */
+public class PaymentSelectorActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
