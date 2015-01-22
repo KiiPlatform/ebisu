ce24b99fc5aa80be83f08b27289878e9e76b47b8
apue <baresi@gmail.com>
2015-01-22 23:54:32 +0800

    TianYang: add win policy

--- YanKon/Android/src/com/kii/yankon/fragments/SettingsFragment.java
+++ YanKon/Android/src/com/kii/yankon/fragments/SettingsFragment.java
@@ -1,20 +1,27 @@
 package com.kii.yankon.fragments;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import com.kii.yankon.activities.GuideActivity;
 import com.kii.yankon.R;
+
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 /**
  * Created by Evan on 14/11/30.
  */
-public class SettingsFragment extends BaseListFragment {
+public class SettingsFragment extends PreferenceFragment implements
+        OnSharedPreferenceChangeListener {
+
+    public static final String ARG_SECTION_NUMBER = "section_number";
+
+    private ListPreference mSyncConfigPref;
+
+    private ListPreference mDefaultHomePref;
+
+    private SharedPreferences mPref;
 
     public static SettingsFragment newInstance(int sectionNumber) {
         SettingsFragment fragment = new SettingsFragment();
@@ -25,30 +32,44 @@ public class SettingsFragment extends BaseListFragment {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        String[] items = getActivity().getResources().getStringArray(R.array.settings);
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(),
-                android.R.layout.simple_list_item_activated_1,
-                android.R.id.text1, items);
-        setListAdapter(adapter);
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        switch (position) {
-            case 1: {
-                Intent intent = new Intent(getActivity(), GuideActivity.class);
-                intent.putExtra(GuideActivity.EXTRA_LAUNCH_NEW, false);
-                startActivity(intent);
-            }
-                break;
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+        mPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mSyncConfigPref = (ListPreference) findPreference("win_policy");
+        setupSyncPolicy();
+    }
+
+    private void setupSyncPolicy() {
+        int winPolicyIndex = 0;
+        try {
+            winPolicyIndex = Integer.parseInt(mSyncConfigPref.getValue());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
+        mSyncConfigPref.setSummary(
+                getResources().getStringArray(R.array.sync_policies)[winPolicyIndex]);
     }
 
+
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        menu.clear();
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        setupSyncPolicy();
     }
 }
