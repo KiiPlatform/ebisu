aae84d63e72fbeccb0b38b2c21ed3a8334acbfbc
unknown <hongwei.guo@kii.com>
2015-03-28 16:58:17 +0800

    Modify buffer size for firmware upgrade

--- MTK/MT7620/Kii_demo/firmware_upgrade.h
+++ MTK/MT7620/Kii_demo/firmware_upgrade.h
@@ -7,8 +7,8 @@
 #define FWUP_DOWNLOAD_FILENAME "image"
 
 #define FWUP_DEFAULT_PORT 80
-#define FWUP_SOCKET_BUF_SIZE 2048
-#define FWUP_HTTP_HEADER_SIZE 512
+#define FWUP_SOCKET_BUF_SIZE 4096
+#define FWUP_HTTP_HEADER_SIZE 1024
 #define FWUP_HOST_SIZE 128
 
 void fwup_upgrade(char* url);
