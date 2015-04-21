d2d290184b8eb72a7500b58e302009aea1c43c83
satoshi <satoshi.kumano@kii.com>
2015-04-21 11:36:21 +0900

    Implemented register api.

--- Linux/kii_device.c
+++ Linux/kii_device.c
@@ -65,3 +65,61 @@ exit:
 	return ret;
 }
 
+int kiiDev_register(kii_t* kii, char* vendorDeviceID, char* deviceType, char* password)
+{
+    char* buf;
+    char thing_data[1024];
+    int ret = -1;
+    kii_error_code_t core_err;
+    kii_state_t state;
+
+    buf = kii->http_context.buffer;
+    sprintf(thing_data,
+            "{\"_vendorThingID\":\"%s\",\"_thingType\":\"%s\",\"_password\":\"%s\"}",
+            vendorDeviceID, deviceType, password);
+    core_err = kii_thing_register(kii, thing_data);
+    if (core_err != KII_E_OK) {
+        goto exit;
+    }
+    do {
+        core_err = kii_run(kii); 
+        state = kii_get_state(kii);
+    } while (state != KII_STATE_IDLE);
+    if (core_err != KII_E_OK) {
+        goto exit;
+    }
+    if(kii->response_code < 200 || 300 <= kii->response_code)
+    {
+        goto exit;
+    }
+    /* parse response */
+    p1 = strstr(buf, "\"_accessToken\"");
+    if(p1 == NULL)
+    {
+        goto exit;
+    }
+    p1 = strstr(p1, ":");
+    if(p1 == NULL)
+    {
+        goto exit;
+    }
+    p1 = strstr(p1, "\"");
+    if(p1 == NULL)
+    {
+        goto exit;
+    }
+    p1 += 1;
+    p2 = strstr(p1, "\"");
+    if(p2 == NULL)
+    {
+        goto exit;
+    }
+    strncpy(kii->author.access_token, p1, p2 - p1);
+    strcpy(kii->author.author_id, vendorDeviceID);
+    ret = 0;
+
+exit:
+    return ret;
+}
+
+/* vim: set tw=4 sw=4 ts=4 et fenc=utf=8: */
