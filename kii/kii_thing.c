0da3251603c95d2e12872d238c22415d859a1c03
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2015-05-21 18:40:30 +0900

    kii_thing_register is refactored.

--- kii/kii_thing.c
+++ kii/kii_thing.c
@@ -73,14 +73,16 @@ int kii_thing_register(
         const char* password)
 {
     char* buf;
-    char* p1;
-    char* p2;
     char thing_data[1024];
+    jsmntok_t tokens[KII_JSON_TOKEN_NUM];
+    jsmntok_t* access_token = NULL;
+    size_t buf_size = 0;
     int ret = -1;
     kii_error_code_t core_err;
     kii_state_t state;
 
     buf = kii->kii_core.http_context.buffer;
+    buf_size = kii->kii_core.http_context.buffer_size;
     sprintf(thing_data,
             "{\"_vendorThingID\":\"%s\",\"_thingType\":\"%s\",\"_password\":\"%s\"}",
             vendor_thing_id, thing_type, password);
@@ -99,24 +101,24 @@ int kii_thing_register(
         goto exit;
     }
     /* parse response */
-    p1 = strstr(buf, "\"_accessToken\"");
-    if(p1 == NULL) {
+    ret = prv_kii_jsmn_get_tokens(kii, buf, buf_size, tokens,
+            sizeof(tokens) / sizeof(tokens[0]));
+    if (ret != 0) {
         goto exit;
     }
-    p1 = strstr(p1, ":");
-    if(p1 == NULL) {
+    if (tokens[0].type != JSMN_OBJECT || tokens[0].size < 2) {
         goto exit;
     }
-    p1 = strstr(p1, "\"");
-    if(p1 == NULL) {
+    ret = prv_kii_jsmn_get_value(buf, buf_size, tokens, "access_token",
+            &access_token);
+    if (ret != 0) {
         goto exit;
     }
-    p1 += 1;
-    p2 = strstr(p1, "\"");
-    if(p2 == NULL) {
+    if (access_token == NULL) {
         goto exit;
     }
-    memcpy(kii->kii_core.author.access_token, p1, p2 - p1);
+    memcpy(kii->kii_core.author.access_token, buf + access_token->start,
+            access_token->end - access_token->start);
     strcpy(kii->kii_core.author.author_id, vendor_thing_id);
     ret = 0;
 
