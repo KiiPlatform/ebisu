df82cd64c719a2f213b5bf89832d57659d3c61d2
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2015-06-11 15:52:03 +0900

    add upload and download body tests.

--- tests/kii_test.cc
+++ tests/kii_test.cc
@@ -73,6 +73,7 @@ TEST(kiiTest, object)
     init(&kii, buffer, 4096, &context);
     initBucket(&bucket);
 
+    memset(objectId, 0x00, KII_OBJECTID_SIZE + 1);
     ret = kii_object_create(&kii, &bucket, "{}", NULL, objectId);
 
     ASSERT_EQ(0, ret);
@@ -125,6 +126,101 @@ TEST(kiiTest, objectWithID)
 
     ASSERT_EQ(0, ret);
     ASSERT_TRUE(NULL != kii.kii_core.response_body);
+
+    ret = kii_object_delete(&kii, &bucket, objectId);
+
+    ASSERT_EQ(0, ret);
+}
+
+TEST(kiiTest, objectBodyOnce)
+{
+    int ret = -1;
+    char* body = "Stop the world!";
+    char buffer[4096];
+    char objectId[KII_OBJECTID_SIZE + 1];
+    kii_t kii;
+    kii_bucket_t bucket;
+    context_t context;
+    unsigned int body_len;
+    unsigned int out_len = 0;
+
+    body_len = strlen(body);
+
+    init(&kii, buffer, 4096, &context);
+    initBucket(&bucket);
+    strcpy(objectId, "my_object");
+
+    ret = kii_object_create_with_id(&kii, &bucket, objectId, "{}", NULL);
+
+    ASSERT_EQ(0, ret);
+
+    ret = kii_object_upload_body_at_once(&kii, &bucket, objectId, "text/plain",
+            body, body_len);
+
+    ASSERT_EQ(0, ret);
+
+    ret = kii_object_download_body_at_once(&kii, &bucket, objectId, &out_len);
+
+    ASSERT_EQ(0, ret);
+    ASSERT_EQ(body_len, out_len);
+    ASSERT_STREQ(body, kii.kii_core.response_body);
+
+    ret = kii_object_delete(&kii, &bucket, objectId);
+
+    ASSERT_EQ(0, ret);
+}
+
+TEST(kiiTest, objectBodyMulti)
+{
+    int ret = -1;
+    char* body = "Stop the world!";
+    char buffer[4096];
+    char objectId[KII_OBJECTID_SIZE + 1];
+    char uploadId[KII_UPLOADID_SIZE + 1];
+    kii_t kii;
+    kii_bucket_t bucket;
+    kii_chunk_data_t chunk;
+    context_t context;
+    unsigned int body_len;
+    unsigned int out_len = 0;
+    unsigned int out_total = 0;
+
+    body_len = strlen(body);
+
+    init(&kii, buffer, 4096, &context);
+    initBucket(&bucket);
+    strcpy(objectId, "my_object");
+
+    ret = kii_object_create_with_id(&kii, &bucket, objectId, "{}", NULL);
+
+    ASSERT_EQ(0, ret);
+
+    memset(uploadId, 0x00, KII_UPLOADID_SIZE + 1);
+    ret = kii_object_init_upload_body(&kii, &bucket, objectId, uploadId);
+
+    ASSERT_EQ(0, ret);
+    ASSERT_NE(0, strlen(uploadId));
+
+    chunk.chunk = body;
+    chunk.body_content_type = "text/plain";
+    chunk.position = 0;
+    chunk.length = chunk.total_length = body_len;
+
+    ret = kii_object_upload_body(&kii, &bucket, objectId, uploadId, &chunk);
+
+    ASSERT_EQ(0, ret);
+
+    ret = kii_object_commit_upload(&kii, &bucket, objectId, uploadId, 1);
+
+    ASSERT_EQ(0, ret);
+
+    ret = kii_object_download_body(&kii, &bucket, objectId, 0, body_len,
+            &out_len, &out_total);
+
+    ASSERT_EQ(0, ret);
+    ASSERT_EQ(body_len, out_len);
+    ASSERT_EQ(body_len, out_total);
+    ASSERT_STREQ(body, kii.kii_core.response_body);
 
     ret = kii_object_delete(&kii, &bucket, objectId);
 
