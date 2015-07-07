94d0f8ce66c73124ae97714ff21b05198b8eb0e1
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2015-07-07 13:38:22 +0900

    Error log is added in prv_kii_json_read_object.

--- kii/kii_json_utils.c
+++ kii/kii_json_utils.c
@@ -1,3 +1,4 @@
+#include "kii.h"
 #include "kii_json_utils.h"
 
 #include <string.h>
@@ -10,8 +11,12 @@ kii_json_parse_result_t prv_kii_json_read_object(
 {
     kii_json_t kii_json;
     kii_json_parse_result_t retval;
+    char error_message[50];
 
     memset(&kii_json, 0, sizeof(kii_json));
+    kii_json.error_string_buff = error_message;
+    kii_json.error_string_length =
+        sizeof(error_message) / sizeof(error_message[0]);
 
 #ifdef KII_JSON_STACK_TOKEN_NUM
     {
@@ -38,6 +43,12 @@ kii_json_parse_result_t prv_kii_json_read_object(
                 json_string_size, fields);
     }
 #endif
+
+    if (retval != KII_JSON_PARSE_SUCCESS) {
+        M_KII_LOG(kii->kii_core.logger_cb(
+                "fail to parse json: result=%d, message=%s",
+                retval, kii_json.error_string_buff));
+    }
     return retval;
 }
 
