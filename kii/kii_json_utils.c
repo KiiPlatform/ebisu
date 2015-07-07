6ba87fca7980db214c7bad3fb02f4feb0186cd09
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2015-07-07 13:32:03 +0900

    KII_JSON_STACK_TOKEN_NUM macro is added.
    If this macro is defined, Kii SDK takes Kii JSON resource on stack. Applications does not need to manage kii_t#kii_json_resource field.

--- kii/kii_json_utils.c
+++ kii/kii_json_utils.c
@@ -13,29 +13,31 @@ kii_json_parse_result_t prv_kii_json_read_object(
 
     memset(&kii_json, 0, sizeof(kii_json));
 
-    if (kii->kii_json_resource == NULL) {
-        kii_json_token_t tokens[256];
+#ifdef KII_JSON_STACK_TOKEN_NUM
+    {
+        kii_json_token_t tokens[KII_JSON_STACK_TOKEN_NUM];
         kii_json.tokens = tokens;
         kii_json.json_token_num = sizeof(tokens) / sizeof(tokens[0]);
         retval = kii_json_read_object(&kii_json, json_string, json_string_size,
                 fields);
-    } else {
+    }
+#else
+    kii_json.tokens = kii->kii_json_resource->tokens;
+    kii_json.json_token_num = kii->kii_json_resource->tokens_num;
+    retval = kii_json_read_object(&kii_json, json_string, json_string_size,
+                fields);
+    if (kii->kii_json_resource_cb != NULL &&
+            retval == KII_JSON_PARSE_SHORTAGE_TOKENS) {
+        if (kii->kii_json_resource_cb(kii->kii_json_resource,
+                        kii_json.json_token_num) != KIIE_OK) {
+            return KII_JSON_PARSE_SHORTAGE_TOKENS;
+        }
         kii_json.tokens = kii->kii_json_resource->tokens;
         kii_json.json_token_num = kii->kii_json_resource->tokens_num;
-        retval = kii_json_read_object(&kii_json, json_string, json_string_size,
-                fields);
-        if (kii->kii_json_resource_cb != NULL &&
-                retval == KII_JSON_PARSE_SHORTAGE_TOKENS) {
-            if (kii->kii_json_resource_cb(kii->kii_json_resource,
-                            kii_json.json_token_num) != KIIE_OK) {
-                return KII_JSON_PARSE_SHORTAGE_TOKENS;
-            }
-            kii_json.tokens = kii->kii_json_resource->tokens;
-            kii_json.json_token_num = kii->kii_json_resource->tokens_num;
-            retval = kii_json_read_object(&kii_json, json_string,
-                    json_string_size, fields);
-        }
+        retval = kii_json_read_object(&kii_json, json_string,
+                json_string_size, fields);
     }
+#endif
     return retval;
 }
 
