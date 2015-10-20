70829cbe0137a2a5c569589e3fdff329175925b8
Riza <riza.alaudin@kii.com>
2015-10-20 18:50:26 +0900

    add install script for dependencies.

---
+++ INTEL/EDISON/install_dep.sh
@@ -0,0 +1,4 @@
+cp -rf ../../kii eclipse_workspace/kii
+cp -rf ../../kii-core eclipse_workspace/kii-core
+cp -rf ../../kii_json eclipse_workspace/kii_json
+cp -rf ../../Linux eclipse_workspace/Linux
