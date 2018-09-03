10f386a2874748a8cfe30116883a5123e5011fad
satoshi <satoshi.kumano@kii.com>
2018-09-03 11:54:15 +0900

    Format code.

--- kii/kii.c
+++ kii/kii.c
@@ -3,32 +3,37 @@
 
 #define KII_SDK_INFO "sn=te;sv=1.2.4"
 
-size_t _cb_write_buff(char *buffer, size_t size, size_t count, void *userdata) {
-  kii_t* kii = (kii_t*)userdata;
-  size_t remain = kii->_rw_buff_size - kii->_rw_buff_written;
-  size_t req_size = size * count;
-  if (remain < req_size) {
-    // Insufficient buffer size.
-    return 0;
-  }
-  memcpy(kii->_rw_buff + kii->_rw_buff_written, buffer, req_size);
-  kii->_rw_buff_written += req_size;
-  return req_size;
+size_t _cb_write_buff(char *buffer, size_t size, size_t count, void *userdata)
+{
+    kii_t *kii = (kii_t *)userdata;
+    size_t remain = kii->_rw_buff_size - kii->_rw_buff_written;
+    size_t req_size = size * count;
+    if (remain < req_size)
+    {
+        // Insufficient buffer size.
+        return 0;
+    }
+    memcpy(kii->_rw_buff + kii->_rw_buff_written, buffer, req_size);
+    kii->_rw_buff_written += req_size;
+    return req_size;
 }
 
-size_t _cb_read_buff(char *buffer, size_t size, size_t count, void *userdata) {
-  kii_t* kii = (kii_t*)userdata;
-  size_t remain = kii->_rw_buff_req_size - kii->_rw_buff_read;
-  if (remain <= 0) {
-    return 0;
-  }
-  size_t to_read = (size * count > remain) ? (remain) : (size * count);
-  memcpy(buffer, kii->_rw_buff + kii->_rw_buff_read, to_read);
-  kii->_rw_buff_read += to_read;
-  return to_read;
+size_t _cb_read_buff(char *buffer, size_t size, size_t count, void *userdata)
+{
+    kii_t *kii = (kii_t *)userdata;
+    size_t remain = kii->_rw_buff_req_size - kii->_rw_buff_read;
+    if (remain <= 0)
+    {
+        return 0;
+    }
+    size_t to_read = (size * count > remain) ? (remain) : (size * count);
+    memcpy(buffer, kii->_rw_buff + kii->_rw_buff_read, to_read);
+    kii->_rw_buff_read += to_read;
+    return to_read;
 }
 
-size_t _cb_write_header(char *buffer, size_t size, size_t count, void *userdata) {
+size_t _cb_write_header(char *buffer, size_t size, size_t count, void *userdata)
+{
     // TODO: implement it later for getting Etag, etc.
     return size * count;
 }
