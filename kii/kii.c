70491eb926134414a0c6cf6a294620f35ebf7c76
satoshi <satoshi.kumano@kii.com>
2018-09-03 11:22:35 +0900

    Added callback for buffering read/write.

--- kii/kii.c
+++ kii/kii.c
@@ -1,5 +1,30 @@
 #include "kii.h"
 
+size_t _cb_write_buff(char *buffer, size_t size, size_t count, void *userdata) {
+  kii_t* kii = (kii_t*)userdata;
+  size_t remain = kii->_rw_buff_size - kii->_rw_buff_written;
+  size_t req_size = size * count;
+  if (remain < req_size) {
+    // Insufficient buffer size.
+    return 0;
+  }
+  memcpy(kii->_rw_buff + kii->_rw_buff_written, buffer, req_size);
+  kii->_rw_buff_written += req_size;
+  return req_size;
+}
+
+size_t _cb_read_buff(char *buffer, size_t size, size_t count, void *userdata) {
+  kii_t* kii = (kii_t*)userdata;
+  size_t remain = kii->_rw_buff_req_size - kii->_rw_buff_read;
+  if (remain <= 0) {
+    return 0;
+  }
+  size_t to_read = (size * count > remain) ? (remain) : (size * count);
+  memcpy(buffer, kii->_rw_buff + kii->_rw_buff_read, to_read);
+  kii->_rw_buff_read += to_read;
+  return to_read;
+}
+
 int kii_api_call(
     kii_t *kii,
     const char *http_method,
