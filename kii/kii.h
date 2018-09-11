df4bec0193627a153e2518ccfcec9c04199de47a
satoshi <satoshi.kumano@kii.com>
2018-09-11 16:05:11 +0900

    Fixed format.

--- kii/kii.h
+++ kii/kii.h
@@ -41,44 +41,44 @@ extern "C" {
 #define KII_TASK_NAME_PING_REQ "ping_req_task"
 #endif
 
-    typedef enum kii_code_t
-    {
-        KII_ERR_OK,
-        KII_ERR_SOCK_CONNECT,
-        KII_ERR_SOCK_CLOSE,
-        KII_ERR_SOCK_SEND,
-        KII_ERR_SOCK_RECV,
-        KII_ERR_HEADER_CALLBACK,
-        KII_ERR_WRITE_CALLBACK,
-        KII_ERR_ALLOCATION,
-        KII_ERR_TOO_LARGE_DATA,
-        KII_ERR_RESP_STATUS,
-        KII_ERR_PARSE_JSON,
-        KII_ERR_FAIL
-    } kii_code_t;
+typedef enum kii_code_t
+{
+    KII_ERR_OK,
+    KII_ERR_SOCK_CONNECT,
+    KII_ERR_SOCK_CLOSE,
+    KII_ERR_SOCK_SEND,
+    KII_ERR_SOCK_RECV,
+    KII_ERR_HEADER_CALLBACK,
+    KII_ERR_WRITE_CALLBACK,
+    KII_ERR_ALLOCATION,
+    KII_ERR_TOO_LARGE_DATA,
+    KII_ERR_RESP_STATUS,
+    KII_ERR_PARSE_JSON,
+    KII_ERR_FAIL
+} kii_code_t;
 
-    /** bool type definition */
-    typedef enum kii_bool_t
-    {
-        KII_FALSE = 0,
-        KII_TRUE
-    } kii_bool_t;
+/** bool type definition */
+typedef enum kii_bool_t
+{
+    KII_FALSE = 0,
+    KII_TRUE
+} kii_bool_t;
 
-    /** represents scope of bucket/ topic. */
-    typedef enum kii_scope_type_t
-    {
-        KII_SCOPE_APP,
-        KII_SCOPE_USER,
-        KII_SCOPE_GROUP,
-        KII_SCOPE_THING
-    } kii_scope_type_t;
+/** represents scope of bucket/ topic. */
+typedef enum kii_scope_type_t
+{
+    KII_SCOPE_APP,
+    KII_SCOPE_USER,
+    KII_SCOPE_GROUP,
+    KII_SCOPE_THING
+} kii_scope_type_t;
 
-    /** represents bucket */
-    typedef struct kii_bucket_t
-    {
-        kii_scope_type_t scope;
-        const char *scope_id;
-        const char *bucket_name;
+/** represents bucket */
+typedef struct kii_bucket_t
+{
+    kii_scope_type_t scope;
+    const char *scope_id;
+    const char *bucket_name;
 } kii_bucket_t;
 
 /** represents topic */
