872af821e698bd4516a339e6e17d7d3601684831
satoshi <satoshi.kumano@kii.com>
2018-09-12 18:20:05 +0900

    Refactored thing registration.

--- kii/kii_thing_impl.c
+++ kii/kii_thing_impl.c
@@ -69,23 +69,30 @@ kii_code_t _register_thing_with_id(
         const char* thing_type)
 {
     khc_set_host(&kii->_khc, kii->_app_host);
+    khc_set_method(&kii->_khc, "POST");
+
     int path_len = snprintf(kii->_rw_buff, kii->_rw_buff_size, "/api/apps/%s/things", kii->_app_id);
     if (path_len >= kii->_rw_buff_size) {
         return KII_ERR_TOO_LARGE_DATA;
     }
     khc_set_path(&kii->_khc, kii->_rw_buff);
-    khc_set_method(&kii->_khc, "POST");
 
     // Request headers.
-    khc_slist* headers = NULL;
-    int x_app_len = snprintf(kii->_rw_buff, kii->_rw_buff_size, "X-Kii-Appid: %s", kii->_app_id);
-    if (x_app_len >= kii->_rw_buff_size) {
-        return KII_ERR_TOO_LARGE_DATA;
+    kii_code_t res = _set_app_id_header(kii);
+    if (res != KII_ERR_OK) {
+        _req_headers_free_all(kii);
+        return res;
     }
-    headers = khc_slist_append(headers, kii->_rw_buff, x_app_len);
-    headers = khc_slist_append(headers, _APP_KEY_HEADER, strlen(_APP_KEY_HEADER));
-    char ct[] = "Content-Type: application/vnd.kii.ThingRegistrationAndAuthorizationRequest+json";
-    headers = khc_slist_append(headers, ct, strlen(ct));
+    res = _set_app_key_header(kii);
+    if (res != KII_ERR_OK) {
+        _req_headers_free_all(kii);
+        return res;
+    }
+    res = _set_content_type(kii, "application/vnd.kii.ThingRegistrationAndAuthorizationRequest+json");
+    if (res != KII_ERR_OK) {
+        _req_headers_free_all(kii);
+        return res;
+    }
 
     // Request body.
     char esc_vid[strlen(vendor_thing_id) * 2 + 1];
@@ -100,25 +107,20 @@ kii_code_t _register_thing_with_id(
         kii->_rw_buff_size,
         "{\"_vendorThingID\":\"%s\", \"_thingType\":\"%s\", \"_password\":\"%s\"}",
         esc_vid, esc_type, esc_pass);
-    if (content_len >= 256) {
-        khc_slist_free_all(headers);
+    if (content_len >= kii->_rw_buff_size) {
+        _req_headers_free_all(kii);
         return KII_ERR_TOO_LARGE_DATA;
     }
 
-    // Content-Length.
-    char cl_h[128];
-    int cl_h_len = snprintf(cl_h, 128, "Content-Length: %d", content_len);
-    if (cl_h_len >= 128) {
-        khc_slist_free_all(headers);
-        return KII_ERR_TOO_LARGE_DATA;
+    res = _set_content_length(kii, content_len);
+    if (res != KII_ERR_OK) {
+        _req_headers_free_all(kii);
+        return res;
     }
-    headers = khc_slist_append(headers, cl_h, cl_h_len);
-    khc_set_req_headers(&kii->_khc, headers);
 
-    kii->_rw_buff_req_size = content_len;
-
+    khc_set_req_headers(&kii->_khc, kii->_req_headers);
     khc_code code = khc_perform(&kii->_khc);
-    khc_slist_free_all(headers);
+    _req_headers_free_all(kii);
 
     return _convert_code(code);
 }
