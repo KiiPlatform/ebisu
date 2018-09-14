dc1b55121dc80d45e85c1a6c26d082d948a71875
satoshi <satoshi.kumano@kii.com>
2018-09-14 17:40:02 +0900

    Implemented kii_api_call_start.

--- kii/kii_api_call.c
+++ kii/kii_api_call.c
@@ -1,4 +1,7 @@
 #include "kii.h"
+#include "kii_impl.h"
+#include "kii_req_impl.h"
+#include <string.h>
 
 kii_code_t kii_api_call_start(
         kii_t* kii,
@@ -7,8 +10,38 @@ kii_code_t kii_api_call_start(
         const char* content_type,
         kii_bool_t set_authentication_header)
 {
-    // TODO: implement it.
-    return -1;
+    khc_set_zero_excl_cb(&kii->_khc);
+    _reset_buff(kii);
+
+    khc_set_host(&kii->_khc, kii->_app_host);
+    khc_set_method(&kii->_khc, http_method);
+    khc_set_path(&kii->_khc, resource_path);
+
+    kii_code_t res = _set_app_id_header(kii);
+    if (res != KII_ERR_OK) {
+        _req_headers_free_all(kii);
+        return res;
+    }
+    res = _set_app_key_header(kii);
+    if (res != KII_ERR_OK) {
+        _req_headers_free_all(kii);
+        return res;
+    }
+    if (content_type != NULL && strlen(content_type) > 0) {
+        res = _set_content_type(kii, content_type);
+        if (res != KII_ERR_OK) {
+            _req_headers_free_all(kii);
+            return res;
+        }
+    }
+    if (set_authentication_header == KII_TRUE) {
+        res = _set_auth_header(kii);
+        if (res != KII_ERR_OK) {
+            _req_headers_free_all(kii);
+            return res;
+        }
+    }
+    return res;
 }
 
 kii_code_t kii_api_call_append_body(
