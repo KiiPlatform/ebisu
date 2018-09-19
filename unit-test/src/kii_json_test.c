e96cab0108ebcfa5586c8d9a41e085ad3ab47d7e
satoshi <satoshi.kumano@kii.com>
2018-09-19 15:00:29 +0900

    Fixing test...

--- unit-test/src/kii_json_test.c
+++ unit-test/src/kii_json_test.c
@@ -546,31 +546,29 @@ TEST(KiiJson, GetObjectTrueByPath) {
     EXPECT_EQ(KII_JSON_TRUE, fields[0].field_copy.boolean_value);
 }
 
-// TEST(KiiJson, GetObjectFalseByPath) {
-//     const char json_string[] = "{\"key1\" : false}";
-//     char buf[256];
-//     kii_json_t kii_json;
-//     kii_json_resource_t resource;
-//     kii_json_token_t tokens[256];
-//     kii_json_field_t fields[2];
+TEST(KiiJson, GetObjectFalseByPath) {
+    const char json_string[] = "{\"key1\" : false}";
+    char buf[256];
 
-//     init_kii_json(&kii_json, &resource, tokens,
-//             sizeof(tokens) / sizeof(tokens[0]));
-//     memset(fields, 0x00, sizeof(fields));
+    kii_json_token_t tokens[256];
+    kii_json_resource_t resource = { tokens, 256 };
 
-//     fields[0].path = "/key1";
-//     fields[0].type = KII_JSON_FIELD_TYPE_BOOLEAN;
-//     fields[1].name = NULL;
+    kii_json_field_t fields[2];
+    memset(fields, 0x00, sizeof(fields));
 
-//     EXPECT_EQ(KII_JSON_PARSE_SUCCESS,
-//             kii_json_read_object(
-//                 &kii_json,
-//                 json_string,
-//                 sizeof(json_string) / sizeof(json_string[0]),
-//                 fields));
-//     EXPECT_EQ(KII_JSON_FIELD_PARSE_SUCCESS, fields[0].result);
-//     EXPECT_EQ(KII_JSON_FALSE, fields[0].field_copy.boolean_value);
-// }
+    fields[0].path = "/key1";
+    fields[0].type = KII_JSON_FIELD_TYPE_BOOLEAN;
+    fields[1].name = NULL;
+
+    kii_json_parse_result_t res = kii_json_read_object(
+        json_string,
+        strlen(json_string),
+        fields,
+        &resource);
+    EXPECT_EQ(KII_JSON_PARSE_SUCCESS, res);
+    EXPECT_EQ(KII_JSON_FIELD_PARSE_SUCCESS, fields[0].result);
+    EXPECT_EQ(KII_JSON_FALSE, fields[0].field_copy.boolean_value);
+}
 
 // TEST(KiiJson, GetObjectSecondLayerStringByPath) {
 //     const char json_string[] = "{\"key1\" : {\"key2\" : \"value1\"}}";
