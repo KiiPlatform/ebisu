5d356765ea475b8fc002750617fef0d13097939e
satoshi <satoshi.kumano@kii.com>
2018-09-19 14:54:56 +0900

    Fixing test...

--- unit-test/src/kii_json_test.c
+++ unit-test/src/kii_json_test.c
@@ -194,30 +194,28 @@ TEST(KiiJson, GetObjectNegativeDotDoubleByName) {
     EXPECT_GE(0.0001, fabs(fields[0].field_copy.double_value + 0.1));
 }
 
-// TEST(KiiJson, GetObjectPositiveEDoubleByName) {
-//     const char json_string[] = "{\"key1\" : 1e-1}";
-//     kii_json_t kii_json;
-//     kii_json_resource_t resource;
-//     kii_json_token_t tokens[256];
-//     kii_json_field_t fields[2];
+TEST(KiiJson, GetObjectPositiveEDoubleByName) {
+    const char json_string[] = "{\"key1\" : 1e-1}";
 
-//     init_kii_json(&kii_json, &resource, tokens,
-//             sizeof(tokens) / sizeof(tokens[0]));
-//     memset(fields, 0x00, sizeof(fields));
+    kii_json_token_t tokens[256];
+    kii_json_resource_t resource = { tokens, 256 };
 
-//     fields[0].name = "key1";
-//     fields[0].type = KII_JSON_FIELD_TYPE_DOUBLE;
-//     fields[1].name = NULL;
+    kii_json_field_t fields[2];
+    memset(fields, 0x00, sizeof(fields));
 
-//     EXPECT_EQ(KII_JSON_PARSE_SUCCESS,
-//             kii_json_read_object(
-//                 &kii_json,
-//                 json_string,
-//                 sizeof(json_string) / sizeof(json_string[0]),
-//                 fields));
-//     EXPECT_EQ(KII_JSON_FIELD_PARSE_SUCCESS, fields[0].result);
-//     EXPECT_GE(0.0001, fabs(fields[0].field_copy.double_value - 0.1));
-// }
+    fields[0].name = "key1";
+    fields[0].type = KII_JSON_FIELD_TYPE_DOUBLE;
+    fields[1].name = NULL;
+
+    kii_json_parse_result_t res = kii_json_read_object(
+        json_string,
+        strlen(json_string),
+        fields,
+        &resource);
+    EXPECT_EQ(KII_JSON_PARSE_SUCCESS, res);
+    EXPECT_EQ(KII_JSON_FIELD_PARSE_SUCCESS, fields[0].result);
+    EXPECT_GE(0.0001, fabs(fields[0].field_copy.double_value - 0.1));
+}
 
 // TEST(KiiJson, GetObjectNegativeEDoubleByName) {
 //     const char json_string[] = "{\"key1\" : -1e-1}";
