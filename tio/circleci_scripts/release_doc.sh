17c3675c25bb048879e5f9a200de092dd091aca5
satoshi <satoshi.kumano@kii.com>
2018-09-20 19:41:31 +0900

    Removed unused script.

--- tio/circleci_scripts/release_doc.sh
+++
@@ -1,9 +0,0 @@
-#! bin/bash
-
-git clone https://$GH_TOKEN_FOR_HTTPS@github.com/KiiPlatform/thing-if-ThingSDK.git
-cd thing-if-ThingSDK
-git checkout gh-pages && git config user.email 'satoshi.kumano@kii.com' && git config user.name 'satoshi kumano'
-git rm -r --ignore-unmatch api-doc && mkdir -p api-doc
-cp -r ../doc/html/ api-doc
-git add api-doc && git commit -m 'updated doc' && git push origin gh-pages
-
