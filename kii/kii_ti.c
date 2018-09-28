54d057deac5fc6dfa3b7e35a7c29cbdb05245310
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2018-09-28 13:21:19 +0900

    change API.

--- kii/kii_ti.c
+++ kii/kii_ti.c
@@ -46,15 +46,8 @@ kii_code_t kii_ti_put_state(
     kii_t* kii,
     size_t content_length,
     KII_CB_READ state_read_cb,
-    kii_bool_t send_to_normalizer)
+    const char* opt_normalizer_host)
 {
     // TODO: implement it.
     return KII_ERR_FAIL;
-}
-
-void kii_ti_set_normalizer_site(
-    kii_t* kii,
-    const char* normalizer_site)
-{
-    // TODO: implement it.
 }
