0e21c0c0601b9abc020c0014ca9ab1aa340d2aa9
satoshi <satoshi.kumano@kii.com>
2018-09-28 19:21:25 +0900

    Added test cases.

--- tests/tio/small_tests/command_parser_test.cpp
+++ tests/tio/small_tests/command_parser_test.cpp
@@ -13,19 +13,55 @@ TEST_CASE( "_get_object_in_array" ) {
 
     char* obj_str = NULL;
     size_t obj_str_len = 0;
-    _cmd_parser_code_t p_res = _get_object_in_array(
-        &resource,
-        NULL,
-        NULL,
-        json_arr,
-        strlen(json_arr),
-        0,
-        &obj_str,
-        &obj_str_len
-        );
-    REQUIRE( p_res == _CMD_PARSE_OK );
-    REQUIRE( obj_str_len == 7);
-    char obj_str_copy[obj_str_len+1];
-    strncpy(obj_str_copy, obj_str, obj_str_len);
-    REQUIRE( strcmp(obj_str_copy, "{\"a\":1}") == 0 );
+
+    SECTION("Get object at index 0") {
+        _cmd_parser_code_t p_res = _get_object_in_array(
+            &resource,
+            NULL,
+            NULL,
+            json_arr,
+            strlen(json_arr),
+            0,
+            &obj_str,
+            &obj_str_len
+            );
+        REQUIRE( p_res == _CMD_PARSE_OK );
+        REQUIRE( obj_str_len == 7);
+        char obj_str_copy[obj_str_len+1];
+        strncpy(obj_str_copy, obj_str, obj_str_len);
+        REQUIRE( strcmp(obj_str_copy, "{\"a\":1}") == 0 );
+    }
+
+    SECTION("Get object at index 1") {
+        _cmd_parser_code_t p_res = _get_object_in_array(
+            &resource,
+            NULL,
+            NULL,
+            json_arr,
+            strlen(json_arr),
+            1,
+            &obj_str,
+            &obj_str_len
+            );
+        REQUIRE( p_res == _CMD_PARSE_OK );
+        REQUIRE( obj_str_len == 7);
+        char obj_str_copy[obj_str_len+1];
+        strncpy(obj_str_copy, obj_str, obj_str_len);
+        REQUIRE( strcmp(obj_str_copy, "{\"b\":2}") == 0 );
+    }
+
+    SECTION("Get object at index 2") {
+        _cmd_parser_code_t p_res = _get_object_in_array(
+            &resource,
+            NULL,
+            NULL,
+            json_arr,
+            strlen(json_arr),
+            2,
+            &obj_str,
+            &obj_str_len
+            );
+        REQUIRE( p_res == _CMD_PARSE_ARRAY_OUT_OF_INDEX );
+    }
+
 }
