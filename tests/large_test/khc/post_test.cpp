abbdc4383eb7b2e6d432c4805a30fbc34d0625cc
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2019-02-26 15:21:28 +0900

    fix khc large tests.

--- tests/large_test/khc/post_test.cpp
+++ tests/large_test/khc/post_test.cpp
@@ -34,9 +34,9 @@ TEST_CASE( "HTTP Post" ) {
 
     // Prepare Req Headers.
     khc_slist* headers = NULL;
-    headers = khc_slist_append(headers, x_kii_appid.c_str(), x_kii_appid.length());
-    headers = khc_slist_append(headers, content_type.c_str(), content_type.length());
-    headers = khc_slist_append(headers, x_kii_appkey.c_str(), x_kii_appkey.length());
+    headers = khc_slist_append_using_cb_alloc(headers, x_kii_appid.c_str(), x_kii_appid.length(), khct::cb::cb_khc_slist_alloc, NULL);
+    headers = khc_slist_append_using_cb_alloc(headers, content_type.c_str(), content_type.length(), khct::cb::cb_khc_slist_alloc, NULL);
+    headers = khc_slist_append_using_cb_alloc(headers, x_kii_appkey.c_str(), x_kii_appkey.length(), khct::cb::cb_khc_slist_alloc, NULL);
 
     khc_set_req_headers(&http, headers);
 
@@ -78,7 +78,7 @@ TEST_CASE( "HTTP Post" ) {
     };
 
     khc_code res = khc_perform(&http);
-    khc_slist_free_all(headers);
+    khc_slist_free_all_using_cb_free(headers, khct::cb::cb_khc_slist_free, NULL);
     REQUIRE( khc_get_status_code(&http) == 200 );
 
     // Parse response body.
