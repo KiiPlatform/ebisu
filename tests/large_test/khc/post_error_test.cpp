020dd224e33edd88a890841396ecb8323f323b73
Francisco A. Lozano <flozano@gmail.com>
2023-06-13 07:05:39 +0200

    Fix KHC post test (#161)
    * Make khc test don't depend on specific app
    
    * Add .envrc to .gitignore
    
    * Drop .vscode stuff
    
    * Verified the content of the buffer and it's as expected.
    
    At some point two writes were done by the server but now just one is
    happening and the assert needs to be changed (at least 1 write)
    
    * Add init code for khc post test user, and explain in README
    
    * Ensure APP_ID is set; remove hard-coded one (legacy, not working)
    
    * Update README.md

--- tests/large_test/khc/post_error_test.cpp
+++ tests/large_test/khc/post_error_test.cpp
@@ -5,20 +5,16 @@
 #include "test_callbacks.h"
 #include "picojson.h"
 #include <sstream>
-#include "../test_env.h"
 
-TEST_CASE( "HTTP Post" ) {
+TEST_CASE( "HTTP Post with error" ) {
     khc http;
     khc_init(&http);
 
-    std::string app_id(APP_ID);
-    std::string username = "pass-1234";
-    std::string password = "1234";
-    std::string path = "/api/apps/" + app_id + "/oauth2/token";
-    std::string host(DEFAULT_SITE);
-    std::string x_kii_appid = "X-Kii-Appid: " + app_id;
-    std::string x_kii_appkey = "X-Kii-Appkey: dummy";
-    std::string content_type = "Content-Type: application/vnd.kii.OauthTokenRequest+json";
+    std::string app_id = "random_app";
+    std::string email = "random_email@domain.local";
+    std::string path = "/api/apps";
+    std::string host = "api-jp.kii.com";
+    std::string content_type = "Content-Type: application/vnd.kii.AppCreationRequest+json";
 
     khc_set_host(&http, host.c_str());
     khc_set_method(&http, "POST");
@@ -26,18 +22,16 @@ TEST_CASE( "HTTP Post" ) {
 
     // Prepare Req Body.
     picojson::object o;
-    o.insert(std::make_pair("username", username));
-    o.insert(std::make_pair("password", password));
-    o.insert(std::make_pair("grant_type", "password"));
+    o.insert(std::make_pair("appID", app_id));
+    o.insert(std::make_pair("emailAddress", email));
+    o.insert(std::make_pair("description", "random"));
     picojson::value v(o);
     std::string req_body = v.serialize();
     size_t body_len = req_body.length();
 
     // Prepare Req Headers.
     khc_slist* headers = NULL;
-    headers = khc_slist_append(headers, x_kii_appid.c_str(), x_kii_appid.length());
     headers = khc_slist_append(headers, content_type.c_str(), content_type.length());
-    headers = khc_slist_append(headers, x_kii_appkey.c_str(), x_kii_appkey.length());
 
     khc_set_req_headers(&http, headers);
 
@@ -83,13 +77,12 @@ TEST_CASE( "HTTP Post" ) {
     io_ctx.on_write = [=, &on_write_called, &oss](char *buffer, size_t size, void *userdata) {
         ++on_write_called;
         oss.write(buffer, size);
-        // printf("ON_WRITE %s", buffer);
         return size;
     };
 
     khc_code res = khc_perform(&http);
     khc_slist_free_all(headers);
-    REQUIRE( khc_get_status_code(&http) == 200 );
+    REQUIRE( khc_get_status_code(&http) == 401 );
 
     // Parse response body.
     auto resp_body = oss.str();
@@ -97,12 +90,12 @@ TEST_CASE( "HTTP Post" ) {
     REQUIRE ( err_str.empty() );
     REQUIRE ( v.is<picojson::object>() );
     picojson::object obj = v.get<picojson::object>();
-    auto access_token = obj.at("access_token");
-    REQUIRE ( access_token.is<std::string>() );
-    REQUIRE ( !access_token.get<std::string>().empty() );
+    auto error_code = obj.at("errorCode");
+    REQUIRE ( error_code.is<std::string>() );
+    REQUIRE ( !error_code.get<std::string>().empty() );
 
     REQUIRE( res == KHC_ERR_OK );
     REQUIRE( on_read_called == 2 );
     REQUIRE( on_header_called > 1 );
-    REQUIRE( on_write_called >= 1 );
+    REQUIRE( on_write_called == 2 );
 }
--- tests/large_test/kii/large_test.h
+++ tests/large_test/kii/large_test.h
@@ -5,25 +5,9 @@
 #include <functional>
 #include "kii.h"
 #include "secure_socket_impl.h"
+#include "../test_env.h"
 
 namespace kiiltest {
-
-// stringify the macro value
-// ref. https://gcc.gnu.org/onlinedocs/gcc-4.8.5/cpp/Stringification.html
-#define XSTR(x) STR(x)
-#define STR(x) #x
-
-#ifndef _DEFAULT_SITE
-const char DEFAULT_SITE[] = "api-jp.kii.com";
-#else
-const char DEFAULT_SITE[] = XSTR(_DEFAULT_SITE);
-#endif
-#ifndef _APP_ID
-// APP Owner: satoshi.kumano@kii.com
-const char APP_ID[] = "b6t9ai81zb3s";
-#else
-const char APP_ID[] = XSTR(_APP_ID);
-#endif
 
 inline void init(
         kii_t* kii,
