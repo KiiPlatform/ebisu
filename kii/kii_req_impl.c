c9c3f4a816d5c0e8ab5264c110c2df67756e25c1
Francisco A. Lozano <flozano@gmail.com>
2023-10-24 13:39:07 +0200

    Use kii_api header functions instead of reimplement in kii_req

--- kii/kii_req_impl.c
+++ kii/kii_req_impl.c
@@ -152,132 +152,48 @@ kii_code_t _set_content_type(
         kii_t* kii,
         const char* content_type)
 {
-    int header_len = snprintf(kii->_rw_buff, kii->_rw_buff_size, "Content-Type: %s", content_type);
-    if (header_len >= kii->_rw_buff_size) {
-        return KII_ERR_TOO_LARGE_DATA;
-    }
-    khc_slist* list = khc_slist_append_using_cb_alloc(kii->_req_headers, kii->_rw_buff, header_len, kii->_cb_slist_alloc, kii->_slist_alloc_data);
-    if (list == NULL) {
-        return KII_ERR_ALLOCATION;
-    }
-    kii->_req_headers = list;
-    return KII_ERR_OK;
+    return kii_api_call_append_header(kii, "Content-Type", content_type);
 }
 
 kii_code_t _set_content_encoding(
         kii_t* kii,
         const char* content_encoding)
 {
-    int header_len = snprintf(kii->_rw_buff, kii->_rw_buff_size, "Content-Encoding: %s", content_encoding);
-    if (header_len >= kii->_rw_buff_size) {
-        return KII_ERR_TOO_LARGE_DATA;
-    }
-    khc_slist* list = khc_slist_append_using_cb_alloc(kii->_req_headers, kii->_rw_buff, header_len, kii->_cb_slist_alloc, kii->_slist_alloc_data);
-    if (list == NULL) {
-        return KII_ERR_ALLOCATION;
-    }
-    kii->_req_headers = list;
-    return KII_ERR_OK;
+    return kii_api_call_append_header(kii, "Content-Encoding", content_encoding);
 }
 
 kii_code_t _set_object_content_type(
         kii_t* kii,
         const char* object_content_type)
 {
-    char ct_key[] = "Content-Type: ";
-    char *ct_value = "application/json";
-    if (object_content_type != NULL && strlen(object_content_type) > 0) {
-        ct_value = (char*)object_content_type;
-    }
-    int header_len = snprintf(kii->_rw_buff, kii->_rw_buff_size, "%s%s", ct_key, ct_value);
-    if (header_len >= kii->_rw_buff_size) {
-        return KII_ERR_TOO_LARGE_DATA;
-    }
-    khc_slist* list = khc_slist_append_using_cb_alloc(kii->_req_headers, kii->_rw_buff, header_len, kii->_cb_slist_alloc, kii->_slist_alloc_data);;
-    if (list == NULL) {
-        return KII_ERR_ALLOCATION;
-    }
-    kii->_req_headers = list;
-    return KII_ERR_OK;
+    return kii_api_call_append_header_or_default(kii, "Content-Type", object_content_type, "application/json");
 }
 
 kii_code_t _set_object_body_content_type(
         kii_t* kii,
         const char* object_body_content_type)
 {
-    char ct_key[] = "Content-Type: ";
-    char *ct_value = "application/octet-stream";
-    if (object_body_content_type != NULL && strlen(object_body_content_type) > 0) {
-        ct_value = (char*)object_body_content_type;
-    }
-    int header_len = snprintf(kii->_rw_buff, kii->_rw_buff_size, "%s%s", ct_key, ct_value);
-    if (header_len >= kii->_rw_buff_size) {
-        return KII_ERR_TOO_LARGE_DATA;
-    }
-    khc_slist* list = khc_slist_append_using_cb_alloc(kii->_req_headers, kii->_rw_buff, header_len, kii->_cb_slist_alloc, kii->_slist_alloc_data);;
-    if (list == NULL) {
-        return KII_ERR_ALLOCATION;
-    }
-    kii->_req_headers = list;
-    return KII_ERR_OK;
+    return kii_api_call_append_header_or_default(kii, "Content-Type", object_body_content_type, "application/octet-stream");
 }
 
 kii_code_t _set_content_length(
         kii_t* kii,
         size_t content_length)
 {
-    size_t cl_size = 128;
-    char cl_h[cl_size];
-    int header_len = snprintf(
-            cl_h,
-            cl_size,
-            "Content-Length: %lld",
-            (long long)content_length);
-    if (header_len >= cl_size) {
-        return KII_ERR_TOO_LARGE_DATA;
-    }
-    khc_slist* list = khc_slist_append_using_cb_alloc(kii->_req_headers, cl_h, header_len, kii->_cb_slist_alloc, kii->_slist_alloc_data);
-    if (list == NULL) {
-        return KII_ERR_ALLOCATION;
-    }
-    kii->_rw_buff_req_size = content_length;
-    return KII_ERR_OK;
+    size_t length_size = 128;
+    char length_value[length_size];
+    snprintf(length_value, length_size, "%lld", (long long)content_length);
+    return kii_api_call_append_header(kii, "Content-Length", length_value);
 }
 
 kii_code_t _set_app_id_header(kii_t* kii)
 {
-    int header_len = snprintf(
-            kii->_rw_buff,
-            kii->_rw_buff_size,
-            "X-Kii-Appid: %s",
-            kii->_app_id);
-    if (header_len >= kii->_rw_buff_size) {
-        return KII_ERR_TOO_LARGE_DATA;
-    }
-    khc_slist* list = khc_slist_append_using_cb_alloc(kii->_req_headers, kii->_rw_buff, header_len, kii->_cb_slist_alloc, kii->_slist_alloc_data);;
-    if (list == NULL) {
-        return KII_ERR_ALLOCATION;
-    }
-    kii->_req_headers = list;
-    return KII_ERR_OK;
+    return kii_api_call_append_header(kii, "X-Kii-Appid", kii->_app_id);
 }
 
 kii_code_t _set_app_key_header(kii_t* kii)
 {
-    int header_len = snprintf(
-            kii->_rw_buff,
-            kii->_rw_buff_size,
-            "X-Kii-Appkey: %s",
-            "k");
-    if (header_len >= kii->_rw_buff_size) {
-        return KII_ERR_TOO_LARGE_DATA;
-    }
-    khc_slist* list = khc_slist_append_using_cb_alloc(kii->_req_headers, kii->_rw_buff, header_len, kii->_cb_slist_alloc, kii->_slist_alloc_data);;
-    if (list == NULL) {
-        return KII_ERR_ALLOCATION;
-    }
-    kii->_req_headers = list;
-    return KII_ERR_OK;
+    return kii_api_call_append_header(kii, "X-Kii-Appkey", "k");
 }
 
 kii_code_t _set_auth_header(kii_t* kii)
@@ -288,20 +204,16 @@ kii_code_t _set_auth_header(kii_t* kii)
 kii_code_t _set_auth_bearer_token(kii_t* kii, const char* token)
 {
     if (strlen(token) > 0) {
+        kii->_rw_buff[0] = '\0';
         int header_len = snprintf(
                 kii->_rw_buff,
                 kii->_rw_buff_size,
-                "Authorization: Bearer %s",
+                "Bearer %s",
                 token);
         if (header_len >= kii->_rw_buff_size) {
             return KII_ERR_TOO_LARGE_DATA;
         }
-        khc_slist* list = khc_slist_append_using_cb_alloc(kii->_req_headers, kii->_rw_buff, header_len, kii->_cb_slist_alloc, kii->_slist_alloc_data);;
-        if (list == NULL) {
-            return KII_ERR_ALLOCATION;
-        }
-        kii->_req_headers = list;
-        return KII_ERR_OK;
+        return kii_api_call_append_header(kii, "Authorization", kii->_rw_buff);
     } else {
         // Nothing to do.
         return KII_ERR_OK;
@@ -314,48 +226,17 @@ kii_code_t _set_if_match(kii_t* kii, const char* etag)
         // Skip.
         return KII_ERR_OK;
     }
-    int header_len = snprintf(
-            kii->_rw_buff,
-            kii->_rw_buff_size,
-            "If-Match: %s",
-            etag);
-    if (header_len >= kii->_rw_buff_size) {
-        return KII_ERR_TOO_LARGE_DATA;
-    }
-    khc_slist* list = khc_slist_append_using_cb_alloc(kii->_req_headers, kii->_rw_buff, header_len, kii->_cb_slist_alloc, kii->_slist_alloc_data);;
-    if (list == NULL) {
-        return KII_ERR_ALLOCATION;
-    }
-    kii->_req_headers = list;
-    return KII_ERR_OK;
+    return kii_api_call_append_header(kii, "If-Match", etag);
 }
 
 kii_code_t _set_req_body(kii_t* kii, const char* body_contents)
 {
     size_t content_len = strlen(body_contents);
-    if (content_len + 1 > kii->_rw_buff_size) {
-        return KII_ERR_TOO_LARGE_DATA;
-    }
-    strncpy(kii->_rw_buff, body_contents, kii->_rw_buff_size);
-
-    kii->_rw_buff_req_size = content_len;
-    return KII_ERR_OK;
+    return kii_api_call_append_body(kii, body_contents, content_len);
 }
 
 kii_code_t _set_m_0_header(kii_t* kii)
 {
-    int header_len = snprintf(
-            kii->_rw_buff,
-            kii->_rw_buff_size,
-            "kii-m: 0");
-    if (header_len >= kii->_rw_buff_size) {
-        return KII_ERR_TOO_LARGE_DATA;
-    }
-    khc_slist* list = khc_slist_append_using_cb_alloc(kii->_req_headers, kii->_rw_buff, header_len, kii->_cb_slist_alloc, kii->_slist_alloc_data);;
-    if (list == NULL) {
-        return KII_ERR_ALLOCATION;
-    }
-    kii->_req_headers = list;
-    return KII_ERR_OK;
+    return kii_api_call_append_header(kii, "kii-m", "0");
 }
 
