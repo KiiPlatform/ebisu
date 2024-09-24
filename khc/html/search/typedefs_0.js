d7642a78228784c4af5f5fb4bbfeedb46bed4273
flozano <flozano@users.noreply.github.com>
2024-09-24 19:29:19 +0000

    deploy: 5cbbe2b6fa48b954046d227d57cd62934dfaad12

--- khc/html/search/typedefs_0.js
+++ khc/html/search/typedefs_0.js
@@ -1,17 +1,17 @@
 var searchData=
 [
-  ['khc_83',['khc',['../khc_8h.html#a610e0ad9827b3e1fb1303d42fe7c636c',1,'khc.h']]],
-  ['khc_5fcb_5fheader_84',['KHC_CB_HEADER',['../khc_8h.html#a28da65fe4a96bed6b6ac8150b9481a20',1,'khc.h']]],
-  ['khc_5fcb_5fread_85',['KHC_CB_READ',['../khc_8h.html#a3b82452e6a8981c670e9eef8450a7ce4',1,'khc.h']]],
-  ['khc_5fcb_5fslist_5falloc_86',['KHC_CB_SLIST_ALLOC',['../khc_8h.html#ab4d588005d08dee796a1c9c830dc4590',1,'khc.h']]],
-  ['khc_5fcb_5fslist_5ffree_87',['KHC_CB_SLIST_FREE',['../khc_8h.html#a32bafb6cf1da963a57d8a167efe9f833',1,'khc.h']]],
-  ['khc_5fcb_5fsock_5fclose_88',['KHC_CB_SOCK_CLOSE',['../khc__socket__callback_8h.html#af54fb240d589fb1044188bd4a81ab78d',1,'khc_socket_callback.h']]],
-  ['khc_5fcb_5fsock_5fconnect_89',['KHC_CB_SOCK_CONNECT',['../khc__socket__callback_8h.html#ae9552240ec64ad637e77cd63c3b65c65',1,'khc_socket_callback.h']]],
-  ['khc_5fcb_5fsock_5frecv_90',['KHC_CB_SOCK_RECV',['../khc__socket__callback_8h.html#a148cf4bd26b43909c9eab71f37b4cda6',1,'khc_socket_callback.h']]],
-  ['khc_5fcb_5fsock_5fsend_91',['KHC_CB_SOCK_SEND',['../khc__socket__callback_8h.html#a96345c64c6bcec1711d2ddaac43224c2',1,'khc_socket_callback.h']]],
-  ['khc_5fcb_5fwrite_92',['KHC_CB_WRITE',['../khc_8h.html#a57e8d1b5da9a4222190c74f8fb87a304',1,'khc.h']]],
-  ['khc_5fcode_93',['khc_code',['../khc_8h.html#a4ed30f721b5f5af350c4187f683964bb',1,'khc.h']]],
-  ['khc_5fslist_94',['khc_slist',['../khc_8h.html#a4a194b9bfb06071b09552f46a2d36c72',1,'khc.h']]],
-  ['khc_5fsock_5fcode_5ft_95',['khc_sock_code_t',['../khc__socket__callback_8h.html#af497c50fbe34dcc5990d93c1e0c61909',1,'khc_socket_callback.h']]],
-  ['khc_5fstate_96',['khc_state',['../khc_8h.html#a1f0056ed7b4fc90f5a3159d5613418c8',1,'khc.h']]]
+  ['khc_85',['khc',['../khc_8h.html#a610e0ad9827b3e1fb1303d42fe7c636c',1,'khc.h']]],
+  ['khc_5fcb_5fheader_86',['KHC_CB_HEADER',['../khc_8h.html#a28da65fe4a96bed6b6ac8150b9481a20',1,'khc.h']]],
+  ['khc_5fcb_5fread_87',['KHC_CB_READ',['../khc_8h.html#a3b82452e6a8981c670e9eef8450a7ce4',1,'khc.h']]],
+  ['khc_5fcb_5fslist_5falloc_88',['KHC_CB_SLIST_ALLOC',['../khc_8h.html#ab4d588005d08dee796a1c9c830dc4590',1,'khc.h']]],
+  ['khc_5fcb_5fslist_5ffree_89',['KHC_CB_SLIST_FREE',['../khc_8h.html#a32bafb6cf1da963a57d8a167efe9f833',1,'khc.h']]],
+  ['khc_5fcb_5fsock_5fclose_90',['KHC_CB_SOCK_CLOSE',['../khc__socket__callback_8h.html#af54fb240d589fb1044188bd4a81ab78d',1,'khc_socket_callback.h']]],
+  ['khc_5fcb_5fsock_5fconnect_91',['KHC_CB_SOCK_CONNECT',['../khc__socket__callback_8h.html#ae9552240ec64ad637e77cd63c3b65c65',1,'khc_socket_callback.h']]],
+  ['khc_5fcb_5fsock_5frecv_92',['KHC_CB_SOCK_RECV',['../khc__socket__callback_8h.html#a148cf4bd26b43909c9eab71f37b4cda6',1,'khc_socket_callback.h']]],
+  ['khc_5fcb_5fsock_5fsend_93',['KHC_CB_SOCK_SEND',['../khc__socket__callback_8h.html#a96345c64c6bcec1711d2ddaac43224c2',1,'khc_socket_callback.h']]],
+  ['khc_5fcb_5fwrite_94',['KHC_CB_WRITE',['../khc_8h.html#a57e8d1b5da9a4222190c74f8fb87a304',1,'khc.h']]],
+  ['khc_5fcode_95',['khc_code',['../khc_8h.html#a4ed30f721b5f5af350c4187f683964bb',1,'khc.h']]],
+  ['khc_5fslist_96',['khc_slist',['../khc_8h.html#a4a194b9bfb06071b09552f46a2d36c72',1,'khc.h']]],
+  ['khc_5fsock_5fcode_5ft_97',['khc_sock_code_t',['../khc__socket__callback_8h.html#af497c50fbe34dcc5990d93c1e0c61909',1,'khc_socket_callback.h']]],
+  ['khc_5fstate_98',['khc_state',['../khc_8h.html#a1f0056ed7b4fc90f5a3159d5613418c8',1,'khc.h']]]
 ];
